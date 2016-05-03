1019419e5afe7e706cb0a9aec462e1bf6d9acf1a
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-03 19:32:59 +0200

    Modificato utils.FileUtils: aggiunti metodi lettura/scrittura oggetti.

--- utils/FileUtils.java
+++ utils/FileUtils.java
@@ -1,8 +1,19 @@
 package utils;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -78,5 +89,89 @@ public class FileUtils {
     public static String[] caricamentoDati() {
         
         return FileUtils.caricamentoDati(STANDARD_FILE);
+    }
+    
+    /**
+     * This method let the user saves to a file a serializable object to access from later.
+     * It is saved to the main path indicated in this class.
+     * 
+     * @param object the object to save.
+     * @param fileName the name of the file.
+     * @return true if object is written correctly, false otherwise.
+     * @throws ClassCastException
+     *      if {@code !(object instanceof Serializable)}
+     *      {@link Serializable}.
+     */
+    public static boolean writeObjectToFile(Object object, String fileName) {
+        
+        if (!(object instanceof Serializable)) {
+            
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE,
+                    "L'oggetto deve implementare l'interfaccia Serializable.");
+            throw new ClassCastException();
+        }
+        
+        try (
+            OutputStream file = new FileOutputStream(MAIN_PATH + fileName);
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output =new ObjectOutputStream(buffer);
+        ){
+            output.writeObject(object);
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * This method let the user saves to a file a serializable object to access from later.
+     * It is saved to the main path and file name indicated in this class.
+     * 
+     * @param object the object to save.
+     * @return true if object is written correctly, false otherwise.
+     * @throws ClassCastException
+     *      if {@code !(object instanceof Serializable)}
+     *      {@link Serializable}.
+     */
+    public static boolean writeObjectToFile(Object object) {
+        
+        return FileUtils.writeObjectToFile(object, STANDARD_FILE);
+    }
+    
+    /**
+     * This method let the user reads from a file a serializable object previously saved.
+     * It is read from the main path indicated in this class.
+     * 
+     * @param fileName the name of the file to read.
+     * @return the object stored in the file.
+     */
+    public static Object readObjectFromFile(String fileName) {
+        
+        Object object = null;
+        try (
+            InputStream file = new FileInputStream(MAIN_PATH + fileName);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input =new ObjectInputStream(buffer);
+        ){
+            object = input.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+        return object;
+    }
+    
+    /**
+     * This method let the user reads from a file a serializable object previously saved.
+     * It is read from the main path and file name indicated in this class.
+     * 
+     * @return the object stored in the file.
+     */
+    public static Object readObjectFromFile() {
+        
+        return FileUtils.readObjectFromFile(STANDARD_FILE);
     }
 }
